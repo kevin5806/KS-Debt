e81d6deb41ca38f31e820092bc4f64529d0698cd
kevin5806 <kevinleoni58@gmail.com>
2023-04-26 18:28:49 +0200

    Try Fix back-End + Fix font-end
    Settings menù, now the state is saved in the local memory so even if you reload the page the menu remain in the same selection.
    Trying to fix the user input used as a query Warning.

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -4,17 +4,28 @@ if ('serviceWorker' in navigator) {
     );
 }
 
-let selected = 1;
+// ################## Menu Selection #######################
+
+// if there is no saved selection save one
+if (!localStorage.getItem('settingMenuSelected')) {
+    localStorage.setItem('settingMenuSelected', '1');
+}
+
+// on load activate the saved selection
+document.getElementById(localStorage.getItem('settingMenuSelected')).classList.add('active');
+document.getElementById(`c${localStorage.getItem('settingMenuSelected')}`).classList.remove('none');
 
 document.querySelectorAll('#menuSelector > li').forEach(li => {
     li.addEventListener('click', event => {
 
-        document.getElementById(selected).classList.remove('active');
+        document.getElementById(localStorage.getItem('settingMenuSelected')).classList.remove('active');
         document.getElementById(li.id).classList.add('active');
 
-        document.getElementById(`c${selected}`).classList.add('none');
+        document.getElementById(`c${localStorage.getItem('settingMenuSelected')}`).classList.add('none');
         document.getElementById(`c${li.id}`).classList.remove('none');
         
-        selected = li.id;
+        localStorage.setItem('settingMenuSelected', `${li.id}`);
     })
 })
+
+// #########################################################
